78d0c2733a265dd13cec9485e8b44197bfb5b213
Pritam Kadam <kpritam@thoughtworks.com>
2019-09-12 12:22:57 +0530

    Use simpler extensions provided by script-dsl

--- src/main/kotlin/aoesw/AoeswSync.kts
+++ src/main/kotlin/aoesw/AoeswSync.kts
@@ -1,40 +1,39 @@
 package aoesw
 
 import esw.ocs.dsl.core.script
-import esw.ocs.dsl.nullable
 import esw.ocs.dsl.params.floatKey
+import esw.ocs.dsl.params.madd
+import esw.ocs.dsl.params.*
 import esw.ocs.dsl.params.taiTimeKey
-import kotlinx.coroutines.async
-import kotlinx.coroutines.future.asCompletableFuture
-
 
 script {
     val prefix = "aoesw.aosq"
     val aoeswOffsetTime = taiTimeKey(name = "scheduledTime")
     val aoeswOffsetXKey = floatKey("x")
     val aoeswOffsetYKey = floatKey("y")
-
     val probeOffsetXKey = floatKey("x")
     val probeOffsetYKey = floatKey("y")
 
     handleSetup("offset") { command ->
-        val scheduledTime = command[aoeswOffsetTime.key].get()
-        val offsetX = command[aoeswOffsetXKey.key].get()
-        val offsetY = command[aoeswOffsetYKey.key].get()
+        val scheduledTime = command(aoeswOffsetTime)
+        val offsetX = command(aoeswOffsetXKey)
+        val offsetY = command(aoeswOffsetYKey)
 
-        val probeCommand =
-                setup(prefix, "scheduledOffset", command.jMaybeObsId().map { it.obsId() }.nullable())
-                        .add(probeOffsetXKey.set(offsetX.jGet(0).get()))
-                        .add(probeOffsetYKey.set(offsetY.jGet(0).get()))
+        val probeOffsetXParam = probeOffsetXKey.set(offsetX(0))
+        val probeOffsetYParam = probeOffsetYKey.set(offsetY(0))
+
+        val probeCommand = setup(prefix, "scheduledOffset", command.obsId)
+                .madd(probeOffsetXParam, probeOffsetYParam)
 
         addSubCommand(command, probeCommand)
         scheduleOnce(scheduledTime.jGet(0).get()) {
-            // fixme: this block should be suspendable
-            val response = async { submitCommandToAssembly("probeAssembly", probeCommand) }
-            response.asCompletableFuture().thenAccept {
-                updateSubCommand(it)
-            }
+            val response = submitAndWaitCommandToAssembly("probeAssembly", probeCommand)
+            updateSubCommand(response)
 
         }
+    }
+
+    handleShutdown {
+        println("shutdown ocs")
     }
 }
